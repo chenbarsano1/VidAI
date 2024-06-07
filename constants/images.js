701804d7ecace5a1e35ab2e8e4cab417c8eb6b25
Chen Barsano <chenbarsano@gmail.com>
2024-06-07 16:02:00 +0300

    Onboarding screen

--- constants/images.js
+++ constants/images.js
@@ -5,5 +5,6 @@ import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import my_logo from "../assets/images/my_logo.png"
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, my_logo };
