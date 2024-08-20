36568d603179e121521113ab50274116923a9804
Chen Barsano <chenbarsano@gmail.com>
2024-08-20 19:49:25 +0300

    create screen & file picker

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -2,6 +2,7 @@ import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { Tabs, Redirect } from 'expo-router'
 import { icons } from '../../constants'
+import { StatusBar } from 'expo-status-bar'
 
 const TabIcon = ({icon, color, name, focused}) => {
     return (
@@ -50,7 +51,7 @@ const TabsLayout = () => {
                     ),
                 }}
             />
-            <Tabs.Screen 
+            {/* <Tabs.Screen 
                 name='bookmark'
                 options={{
                     title: 'Bookmark',
@@ -64,7 +65,7 @@ const TabsLayout = () => {
                         />
                     ),
                 }}
-            />
+            /> */}
             <Tabs.Screen 
                 name='create'
                 options={{
@@ -96,6 +97,7 @@ const TabsLayout = () => {
                 }}
             />
         </Tabs>
+        <StatusBar backgroundColor='161622' style='light'/>
     </>
   )
 }
